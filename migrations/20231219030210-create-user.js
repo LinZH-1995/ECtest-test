01364c046efef191f8035aebd80bc1393832b020
LinZH-1995 <lin84610@gmail.com>
2023-12-19 11:45:26 +0800

    create user model

---
+++ migrations/20231219030210-create-user.js
@@ -0,0 +1,43 @@
+'use strict'
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      email: {
+        type: Sequelize.STRING
+      },
+      password: {
+        type: Sequelize.STRING
+      },
+      created_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updated_at: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    })
+  },
+  async down (queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users')
+  }
+}
+
+/*
+建立 table 的 SQL
+
+CREATE TABLE Users (
+  id SERIAL PRIMARY KEY,
+  email VARCHAR(255)
+  password VARCHAR(255)
+  created_at timestamptz NOT NULL,
+  updated_at timestamptz NOT NULL
+  )
+*/
