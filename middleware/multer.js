bd09fcc14df914f54f391b0ce09de63cfdb6b3ff
LinZH-1995 <lin84610@gmail.com>
2024-01-19 16:22:03 +0800

    modify multer.js

--- middleware/multer.js
+++ middleware/multer.js
@@ -29,6 +29,39 @@ const localFileHandler = (file) => {
   })
 }
 
+// 使用  async + await + fs.promises
+// const localFileHandler = async (file) => {
+//   try {
+//     if (!file) return null
+//     const fileName = `uploads/${file.originalname}`
+//     const data = await fs.promises.readFile(file.path)
+//     await fs.promises.writeFile(fileName, data)
+//     await fs.promises.rm(file.path) // 上傳完成後刪除temp資料夾臨時檔案
+//     return `/${fileName}`
+//   } catch (error) {
+//     return error
+//   }
+// }
+
+// 使用  createReadStream + createWriteStream + pipe
+// const { finished } = require('node:stream/promises')
+// const localFileHandler = async (file) => {
+//   try {
+//     if (!file) return null
+//     const fileName = `uploads/${file.originalname}`
+//     const readStream = fs.createReadStream(file.path)
+//     const writeStream = fs.createWriteStream(fileName)
+//     const pipeline = readStream.pipe(writeStream)
+//     console.log('======', readStream.readableEnded, '===before finish===', writeStream.writableFinished, '======')
+//     await finished(pipeline)
+//     console.log('======', readStream.readableEnded, '===after finish===', writeStream.writableFinished, '======')
+//     await fs.promises.rm(file.path) // 上傳完成後刪除temp資料夾臨時檔案
+//     return `/${fileName}`
+//   } catch (error) {
+//     return error
+//   }
+// }
+
 module.exports = {
   upload,
   localFileHandler
